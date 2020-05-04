28352eb4929f6508fdfbc48c26438cd9431c8ff7
torayeff <torayeff@gmail.com>
2020-05-04 19:22:26 +0200

    social links

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ const Footer = () => (
   <div className="footer-basic">
     <footer className="border rounded-0">
       <div className="social">
-        <a href="https://github.com/torayeff" target="_blank"><i className="icon ion-social-github"/></a>
-        <a href="https://twitter.com/torayeff" target="_blank"><i className="icon ion-social-twitter"/></a>
+        <a href="https://github.com/torayeff/dlquiz/" target="_blank"><i className="icon ion-social-github"/></a>
+        <a href="https://twitter.com/challengesai" target="_blank"><i className="icon ion-social-twitter"/></a>
       </div>
     </footer>
   </div>
