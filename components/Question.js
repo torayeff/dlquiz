8e34e22ce9dd99671ba6ec7d9ae89c0818953ba9
torayeff <torayeff@gmail.com>
2020-05-15 19:59:46 +0200

    AnswerForm component

--- components/Question.js
+++ components/Question.js
@@ -1,37 +1,40 @@
 import React from "react";
-import AnswerChoices from "./AnswerChoices";
+import AnswerForm from "./AnswerForm";
 import renderMathInElement from "../modules/auto-render";
 
 class Question extends React.Component {
   constructor(props) {
     super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   componentDidMount() {
     renderMathInElement(document.getElementById("question"));
   }
 
+  onSubmit(answers) {
+    console.log(answers);
+  }
+
   render() {
     return (
-      <div id="question" className="question">
-        <div
-          id="question-text"
-          className="text-justify"
-          dangerouslySetInnerHTML={{__html: this.props.question.text}}/>
+      <div id="question" className="question-container">
+        <form id={this.props.question.id}>
+          <div
+            id="question-text"
+            className="text-justify"
+            dangerouslySetInnerHTML={{__html: this.props.question.text}}/>
 
-        <form className="dlq-question-form" id={this.props.question.id}>
-          <AnswerChoices id={this.props.question.id}
-                         answers={this.props.question.answers}
-                         type={this.props.question.type}
-                         onChange={this.onChange}/>
-        </form>
-
-        <div className="dlq-question-source">
           <small className="text-black-50">
-            <em>Source: {this.props.question.source}</em>
+            <i>Source: {this.props.question.source}</i>
           </small>
-        </div>
 
+          <AnswerForm id={this.props.question.id}
+                      answers={this.props.question.answers}
+                      type={this.props.question.type}
+                      onSubmit={this.onSubmit}/>
+        </form>
       </div>
     );
   }
