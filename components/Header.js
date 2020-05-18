a6c05df083bcb7afe614543ccef71e1ddd32dc77
torayeff <torayeff@gmail.com>
2020-05-18 15:05:19 +0200

    link to refresh

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,9 @@
-import Link from "next/link";
-
 const Header = () => (
   <div>
     <div className="header-blue">
       <nav className="navbar navbar-light navbar-expand-md">
         <div className="container-fluid">
-          <Link href="/">
-            <a className="navbar-brand">DLQuiz</a>
-          </Link>
+          <a href="/" className="navbar-brand">DLQuiz</a>
           <button className="navbar-toggler" data-toggle="collapse">
             <span className="sr-only">Toggle navigation</span>
             <span className="navbar-toggler-icon"/></button>
